ff492397f3f3419cc85beb6ce46ad702d4bd3f88
mitchellwest <Mitchell-west@hotmail.com>
2017-04-10 03:51:16 +1000

    Update keyPressed.c
    updated

--- keyPressed.c
+++ keyPressed.c
@@ -1,14 +1,31 @@
 //includes
 
-int keyPressed(key){
-    //
-    int motorCmd = [0,0,0,0,0]; //init motor array
-    switch(key){
-        case "UPARROW":
-            // motor["corresponding key"] = value;
-        case "DOWNARROW":
-            //do this
-            //etc, will change using gamepad
-    };
+int[] keyPressed(){
+	
+	int[] motorCmd = [];
+	
+	switch(key) {
+		case "UP":
+			motorCmd += [0, 1];
+		case "DOWN":
+			motorCmd += [0, -1];
+		case "LEFT":
+			motorCmd += [1, 1];
+		case "RIGHT":
+			motorCmd += [1, -1];
+		case "W":
+			motorCmd += [2, 1];
+		case "S":
+			motorCmd += [2, -1];
+		case "A":
+			motorCmd += [3, 1];
+		case "D":
+			motorCmd += [3, -1];
+		case "Q":
+			motorCmd += [4, 1];
+		case "E":
+			motorCmd += [4, -1];
+	}
     return motorCmd;
+	
 };

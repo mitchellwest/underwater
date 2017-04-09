d5e62dacf8c52f653624cb34b7376849eba28c16
mitchellwest <Mitchell-west@hotmail.com>
2017-04-10 03:50:15 +1000

    Update executeCmd.c
    fix for input

--- executeCmd.c
+++ executeCmd.c
@@ -3,137 +3,145 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-void executeCmd(int number, int value){
-	switch (number){
-		//motor 0
-		case 0:
-			switch(value){
-				case -1:
-					//motor off
-					PORTD &= ~(PIND6 | PIND7);
-					PORTB &= ~PINB0;
-					//operate motor clockwise
-					PORTD |= PIND6;  //(1<<6); //0b01000000 PD6 = High - enable motor for B0/D7 but disable D7
-					PORTB |= PINB0;   //(1<<0); //0b00000001; // PB0 = High - motor B0 direction
+void executeCmd(int[] motorCmd){
+	int[] number = [];
+	int[] value = [];
+	for (i=0; i<length(motorCmd); i++) {
+		int[] number += motorCmd[i][0];
+		int[] value += motorCmd[i][1];
+	}
+	for (i=0; i<length(motorCmd); i++)
+		switch (number[i]){
+			//motor 0
+			case 0:
+				switch(value[i]){
+					case -1:
+						//motor off
+						PORTD &= ~(PIND6 | PIND7);
+						PORTB &= ~PINB0;
+						//operate motor clockwise
+						PORTD |= PIND6;  //(1<<6); //0b01000000 PD6 = High - enable motor for B0/D7 but disable D7
+						PORTB |= PINB0;   //(1<<0); //0b00000001; // PB0 = High - motor B0 direction
+						break;
+					case 1:
+						//motor off
+						PORTD &= ~(PIND6 | PIND7);
+						PORTB &= ~PINB0;
+						//operate motor counter-clockwise
+						PORTD |= PIND7 | PIND6;   //(1<<7) | (1<<6); // 0b11000000; // PD6 = High - enable motor for B0/D7 but disable D7
+						break;
+					case 0:
+						//motor off
+						PORTD &= ~(PIND6 | PIND7); // PD6 = High - enable motor for B0/D7 but disable D7
+						PORTB &= ~PINB0; // PB0 = High - motor B0 direction
+						break;
+					}
 					break;
-				case 1:
-					//motor off
-					PORTD &= ~(PIND6 | PIND7);
-					PORTB &= ~PINB0;
-					//operate motor counter-clockwise
-					PORTD |= PIND7 | PIND6;   //(1<<7) | (1<<6); // 0b11000000; // PD6 = High - enable motor for B0/D7 but disable D7
+			//motor 1
+			case 1:
+				switch(value){
+					case -1:
+						//motor off
+						PORTD &= ~PIND5;
+						PORTB &= ~(PINB6 | PINB7);
+						//operate motor clockwise
+						PORTD |= PIND5;  
+						PORTB |= PINB6; 
+						break;  
+					case 1:
+						//motor off
+						PORTD &= ~PIND5;
+						PORTB &= ~(PINB6 | PINB7);
+						//operate motor counter-clockwise
+						PORTD |= PIND5;   
+						PORTB |= PINB7;
+						break;
+					case 0:
+						//motor off
+						PORTD &= ~PIND5;
+						PORTB &= ~(PINB6 | PINB7);
+						break;
+					}
 					break;
-				case 0:
-					//motor off
-					PORTD &= ~(PIND6 | PIND7); // PD6 = High - enable motor for B0/D7 but disable D7
-					PORTB &= ~PINB0; // PB0 = High - motor B0 direction
-					break;
+			//motor 2
+			case 2:
+				switch(value){
+					case -1:
+						//motor off
+						PORTC &= ~(PINC0 | PINC1);
+						PORTB &= ~PINB1;
+						//operate motor clockwise
+						PORTC |= PINC1;
+						PORTB |= PINB1;
+						break;
+					case 1:
+						//motor off
+						PORTC &= ~(PINC0 | PINC1);
+						PORTB &= ~PINB1;
+						//operate motor counter-clockwise
+						PORTC |= PINC0;
+						PORTB |= PINB1;
+						break;
+					case 0:
+						//motor off
+						PORTC &= ~(PINC0 | PINC1);
+						PORTB &= ~PINB1;
+						break;
 				}
 				break;
-		//motor 1
-		case 1:
-			switch(value){
-				case -1:
-					//motor off
-					PORTD &= ~PIND5;
-					PORTB &= ~(PINB6 | PINB7);
-					//operate motor clockwise
-					PORTD |= PIND5;  
-					PORTB |= PINB6; 
-					break;  
-				case 1:
-					//motor off
-					PORTD &= ~PIND5;
-					PORTB &= ~(PINB6 | PINB7);
-					//operate motor counter-clockwise
-					PORTD |= PIND5;   
-					PORTB |= PINB7;
-					break;
-				case 0:
-					//motor off
-					PORTD &= ~PIND5;
-					PORTB &= ~(PINB6 | PINB7);
-					break;
+			//motor 3
+			case 3:
+				switch(value){
+					case -1:
+						//motor off
+						PORTC &= ~(PINC2 | PINC3);
+						PORTB &= ~PINB2;
+						//operate motor clockwise
+						PORTC |= PINC2;
+						PORTB |= PINB2;
+						break;
+					case 1:
+						//motor off
+						PORTC &= ~(PINC2 | PINC3);
+						PORTB &= ~PINB2;
+						//operate motor counter-clockwise
+						PORTC |= PINC3;
+						PORTB |= PINB2;
+						break;
+					case 0:
+						//motor off
+						PORTC &= ~(PINC2 | PINC3);
+						PORTB &= ~PINB2;
+						break;
 				}
 				break;
-		//motor 2
-		case 2:
-			switch(value){
-				case -1:
-					//motor off
-					PORTC &= ~(PINC0 | PINC1);
-					PORTB &= ~PINB1;
-					//operate motor clockwise
-					PORTC |= PINC1;
-					PORTB |= PINB1;
-					break;
-				case 1:
-					//motor off
-					PORTC &= ~(PINC0 | PINC1);
-					PORTB &= ~PINB1;
-					//operate motor counter-clockwise
-					PORTC |= PINC0;
-					PORTB |= PINB1;
-					break;
-				case 0:
-					//motor off
-					PORTC &= ~(PINC0 | PINC1);
-					PORTB &= ~PINB1;
-					break;
-			}
-			break;
-		//motor 3
-		case 3:
-			switch(value){
-				case -1:
-					//motor off
-					PORTC &= ~(PINC2 | PINC3);
-					PORTB &= ~PINB2;
-					//operate motor clockwise
-					PORTC |= PINC2;
-					PORTB |= PINB2;
-					break;
-				case 1:
-					//motor off
-					PORTC &= ~(PINC2 | PINC3);
-					PORTB &= ~PINB2;
-					//operate motor counter-clockwise
-					PORTC |= PINC3;
-					PORTB |= PINB2;
-					break;
-				case 0:
-					//motor off
-					PORTC &= ~(PINC2 | PINC3);
-					PORTB &= ~PINB2;
-					break;
-			}
-			break;
-			/*
-		//motor 4 PWM
-		case 4:
-			switch(value){
-				case -1:
-					//motor off
-					PORTC &= ~(PINC2 | PINC3);
-					PORTB &= ~PINB2;
-					//operate motor clockwise
-					PORTC |= PINC2;
-					PORTB |= PINB2;
-					break;
-				case 1:
-					//motor off
-					PORTC &= ~(PINC2 | PINC3);
-					PORTB &= ~PINB2;
-					//operate motor counter-clockwise
-					PORTC |= PINC3;
-					PORTB |= PINB2;
-					break;
-				case 0:
-					//motor off
-					PORTC &= ~(PINC2 | PINC3);
-					PORTB &= ~PINB2;
-					break;
-			}
-			break; */
+				/*
+			//motor 4 PWM
+			case 4:
+				switch(value){
+					case -1:
+						//motor off
+						PORTC &= ~(PINC2 | PINC3);
+						PORTB &= ~PINB2;
+						//operate motor clockwise
+						PORTC |= PINC2;
+						PORTB |= PINB2;
+						break;
+					case 1:
+						//motor off
+						PORTC &= ~(PINC2 | PINC3);
+						PORTB &= ~PINB2;
+						//operate motor counter-clockwise
+						PORTC |= PINC3;
+						PORTB |= PINB2;
+						break;
+					case 0:
+						//motor off
+						PORTC &= ~(PINC2 | PINC3);
+						PORTB &= ~PINB2;
+						break;
+				}
+				break; */
+		}
 	}
 }
